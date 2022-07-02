b8e1d2cc557cd4435b6fa4285a66b79ee62e3b75
Jed-Tapiador <doom.slaver2@gmail.com>
2022-07-02 16:46:16 +0800

    add dummy content for work exp

--- src/components/Work.js
+++ src/components/Work.js
@@ -6,7 +6,32 @@ class Work extends Component {
   render() {
     return (
       <div className="work">
-        <h1>hi there Work</h1>
+        <h2>Work Experience</h2>
+        
+        <div className="work-group">
+          <div className="work-left">
+            <h3>Coca-Cola Beverages Philippines Inc</h3>
+            <h4>Davao City Philippines</h4>
+            <h4>2000 - 1999</h4>
+          </div>
+          <div className="work-right">
+            <h3>Account managing officer of the East Coast</h3>
+            <h4>Descriptions go here</h4>
+          </div>
+        </div>
+
+        <div className="work-group">
+          <div className="work-left">
+            <h3>Coca-Cola Beverages Philippines Inc</h3>
+            <h4>Davao City Philippines</h4>
+            <h4>2000 - 1999</h4>
+          </div>
+          <div className="work-right">
+            <h3>Account managing officer of the East Coast</h3>
+            <h4>Descriptions go here</h4>
+          </div>
+        </div>
+        
       </div>
     );
   }
