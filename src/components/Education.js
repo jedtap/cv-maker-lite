5f470f45d8a36bf1f4e5404189b8a0c5f0c68807
Jed-Tapiador <doom.slaver2@gmail.com>
2022-07-02 17:03:08 +0800

    dummy content complete

--- src/components/Education.js
+++ src/components/Education.js
@@ -6,7 +6,32 @@ class Education extends Component {
   render() {
     return (
       <div className="education">
-        <h1>hi there Education</h1>
+        <h2>Education</h2>
+
+        <div className="education-group">
+          <div className="education-left">
+            <h3>De La Salle University Manila</h3>
+            <h4>1888 - 1920</h4>
+          </div>
+
+          <div className="education-right">
+            <h3>Bachelor of Science in Mechanical Engr.</h3>
+            <h4>Relevant honors, projects done</h4>
+          </div>
+        </div>
+
+        <div className="education-group">
+          <div className="education-left">
+            <h3>De La Salle University Manila</h3>
+            <h4>1888 - 1920</h4>
+          </div>
+
+          <div className="education-right">
+            <h3>Bachelor of Science in Mechanical Engr.</h3>
+            <h4>Relevant honors, projects done</h4>
+          </div>
+        </div>
+
       </div>
     );
   }
--- src/components/Skills.js
+++ src/components/Skills.js
@@ -6,7 +6,13 @@ class Skills extends Component {
   render() {
     return (
       <div className="skills">
-        <h1>hi there Skills</h1>
+        <h2>Relevant Skills</h2>
+        <div className="skills-group">
+          <h3>JavaScript</h3>
+          <h3>React JS</h3>
+          <h3>Project Management</h3>
+          <h3>Portfolio Management</h3>
+        </div> 
       </div>
     );
   }
