cfda31beb4a0bfceab738e8747afafc4213c7143
Jed-Tapiador <doom.slaver2@gmail.com>
2022-07-03 16:36:06 +0800

    + beside skills header works. Remove all dummy content

--- src/components/Education.js
+++ src/components/Education.js
@@ -74,32 +74,6 @@ class Education extends Component {
           <h2>Education</h2>
           <div onClick={this.educationNewEntry} className="education-add">+</div>
         </div>
-
-        <div className="education-group">
-          <div className="education-left">
-            <input className="form-control-plaintext education-subtitle" type="text" placeholder="University/College Name" value="De La Salle University" />
-            <input className="form-control-plaintext education-caption" type="text" placeholder="YEAR - YEAR" value="1989 - 2010" />
-          </div>
-          <div className="education-right">
-            <input className="form-control-plaintext education-subtitle" type="text" placeholder="Degree/Certificate Received" value="Bachelor of Science in Mechanical Engr" />
-            <textarea className="form-control-plaintext" placeholder="Showcase your achievements here!" rows="2" value="Foresee over 44 projects and initiatives between Technology and Business" /> 
-          </div>
-          <div className="education-delete"> x </div>
-        </div>
-
-        <div className="education-group">
-          <div className="education-left">
-            <input className="form-control-plaintext education-subtitle" type="text" placeholder="University/College Name" value="De La Salle University" />
-            <input className="form-control-plaintext education-caption" type="text" placeholder="YEAR - YEAR" value="1989 - 2010" />
-          </div>
-          <div className="education-right">
-            <input className="form-control-plaintext education-subtitle" type="text" placeholder="Degree/Certificate Received" value="Bachelor of Science in Mechanical Engr" />
-            <textarea className="form-control-plaintext" placeholder="Showcase your achievements here!" rows="2" value="Foresee over 44 projects and initiatives between Technology and Business" /> 
-          </div>
-          <div className="education-delete"> x </div>
-
-        </div>
-
       </div>
     );
   }
--- src/components/Skills.js
+++ src/components/Skills.js
@@ -3,32 +3,40 @@ import '../styles/Skills.css';
 
 class Skills extends Component {
 
+  skillsNewEntry = () => {
+    // Goal is to generate this DOM:
+    // <span>
+    //   <input className="form-control-plaintext" type="text" placeholder="Your Skill Here"/>
+    //   <div className="skills-delete"> x </div>
+    // </span>
+
+    const skillsSection = document.querySelector(".skills-group");
+    let skillsSpan, skillsInput, skillsDelete;
+    skillsSpan = document.createElement("span");
+
+    skillsInput = document.createElement("input");
+    skillsInput.setAttribute("class","form-control-plaintext");
+    skillsInput.setAttribute("type","text");
+    skillsInput.setAttribute("placeholder","Your Skill Here");
+
+    skillsDelete = document.createElement("div");
+    skillsDelete.appendChild(document.createTextNode(" x "));
+    skillsDelete.setAttribute("class","skills-delete");
+
+    skillsSpan.appendChild(skillsInput);
+    skillsSpan.appendChild(skillsDelete);
+    skillsSection.appendChild(skillsSpan);
+  };
+
+
   render() {
     return (
       <div className="skills">
         <div className="skills-header">
           <h2>Relevant Skills</h2>
-          <div className="skills-add">+</div>
+          <div onClick={this.skillsNewEntry}  className="skills-add">+</div>
         </div>
-
-        <div className="skills-group">
-          <span>
-            <input className="form-control-plaintext" type="text" placeholder="Your Skill Here" value="JavaScript" />
-            <div className="skills-delete"> x </div>
-          </span>
-          <span>
-            <input className="form-control-plaintext" type="text" placeholder="Your Skill Here" value="React JS" />
-            <div className="skills-delete"> x </div>
-          </span>
-          <span>
-            <input className="form-control-plaintext" type="text" placeholder="Your Skill Here" value="Project Management" />
-            <div className="skills-delete"> x </div>
-          </span>
-          <span>
-            <input className="form-control-plaintext" type="text" placeholder="Your Skill Here" value="Portfolio Management" />
-            <div className="skills-delete"> x </div>
-          </span>
-        </div> 
+        <div className="skills-group"></div> 
       </div>
     );
   }
